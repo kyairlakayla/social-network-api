4273f8822316bcf590f249b424d11cee0a27592d
Kayla <kyairlakayla.davis@gmail.com>
2021-11-23 15:56:19 -0500

    create models

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 const express = require('express');
 const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    // useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if (err) throw err;
+    console.log('Connected to MongoDB!!!')
+});
+
+//app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));

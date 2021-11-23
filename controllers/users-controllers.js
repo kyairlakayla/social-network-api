d3d6bd64a8affc1c9b8ce7c637fac4d4cd6f2269
Kayla <kyairlakayla.davis@gmail.com>
2021-11-23 16:26:44 -0500

    start user functions

---
+++ controllers/users-controllers.js
@@ -0,0 +1,37 @@
+const { Users, Thoughts } = require('../models');
+
+const usersController = {
+
+    getAllUser(req, res) {
+        Users.find({})
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+
+    getUserById({ params }, res) {
+        Users.findOne({ _id: params.userId })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+}
+
+
+module.exports = usersController;
